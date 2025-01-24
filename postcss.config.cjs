1a3d6429e6f1e676ab7133a004fbc0bf1504acb6
Anmol <anmol.volt@gmail.com>
2025-01-25 00:38:03 +0530

    x

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'), // Tailwind CSS PostCSS plugin
+    require('autoprefixer'),
+  ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}', // Ensure this includes all your JS/JSX files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
